fbc19df89f792329b5ce0f41b4bf5b062404b472
김아인 <skyblue7462@naver.com>
2025-12-12 18:20:01 +0900

    6번에서 하란대로 Bob branch 만들고 A.java, B.java 수정함

--- TaskGit/src/taskgit/A.java
+++ TaskGit/src/taskgit/A.java
@@ -5,6 +5,7 @@ public class A {
 	public static void main(String[] args)
 	{
 		System.out.println("처음 만듦 -> 그 다음은 뭐해야 함?");
+		System.out.println("지금 Bob branch를 만들었고 (5번) 6번대로 수정하는중, 근데 master branch에서 c 수정한거 3번이 아니라 4번이었음 ㅡㅡ");
 	}
 
 }
