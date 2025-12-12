e6c35f3aead1e7feb9fa43f72836b40ef3dc072a
김아인 <skyblue7462@naver.com>
2025-12-12 18:13:25 +0900

    alice branch 만들고 a.java, c.java 수정함

--- TaskGit/src/taskgit/A.java
+++ TaskGit/src/taskgit/A.java
@@ -5,6 +5,7 @@ public class A {
 	public static void main(String[] args)
 	{
 		System.out.println("처음 만듦 -> 그 다음은 뭐해야 함?");
+		System.out.println("Alice branch 만들고 지금 2번 수정하는 단계임.");
 	}
 
 }
--- TaskGit/src/taskgit/C.java
+++ TaskGit/src/taskgit/C.java
@@ -5,6 +5,7 @@ public class C {
 	public void printC()
 	{
 		System.out.println("C를 호출해서 프린트 했냐?");
+		System.out.println("일단 어디에서든 아직 호출 안했지만 2번 진행 과정에서 C 수정");
 	}
 
 }
