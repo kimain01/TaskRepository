9c4adf375c3b01d7af605f8502d9eb7ee8ce1c9f
김아인 <skyblue7462@naver.com>
2025-12-12 18:15:26 +0900

    master branch에서 c.java 수정함 -> 3번 한거임.

--- TaskGit/src/taskgit/C.java
+++ TaskGit/src/taskgit/C.java
@@ -5,6 +5,7 @@ public class C {
 	public void printC()
 	{
 		System.out.println("C를 호출해서 프린트 했냐?");
+		System.out.println("지금 3번임, 근데 alice branch에서 얘 수정했었음, 아마 그거 merge? 할 때 그거 날 듯.. 그 뭐더라");
 	}
 
 }
